c9c70ddef2fd678e22f6e26262edddc48107886e
JasmineMenu <jasmine.menu@hotmail.com>
2026-04-23 16:27:39 +0200

    nieuwe

--- js/pages/page-sounds/page-sounds-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-angular.module('app.controllers')
-
-.controller('PageSoundsController', ['$scope', 'Store', '$stateParams', '$rootScope', '$ionicHistory',
-    function ($scope, Store, $stateParams, $rootScope, $ionicHistory) {
-      $scope.favorites = Store.getFavoriteSounds();
-      $scope.category = Store.getCategoryByIndex($stateParams.theme);
-    }
-]);
